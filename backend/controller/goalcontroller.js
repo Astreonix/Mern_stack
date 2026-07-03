3994573be9acd1a2542443a2d9a441e259c27714
Astreonix <sheikhmehrali5@gmail.com>
2026-07-03 12:59:07 +0500

    new

---
+++ backend/controller/goalcontroller.js
@@ -0,0 +1,51 @@
+//@desciption : get goal
+//route : GET/api/goals
+//@ access : private
+
+
+
+const getGoals = (req, res) => {
+
+     res.status(200).json({ message: 'Get goals'})
+
+}
+
+//@desciption : set goal
+//route : SET/api/goals
+//@ access : private
+
+
+
+const setGoal = (req, res) => {
+
+     res.status(200).json({ message: 'Get goals'})
+
+}
+
+//@desciption : get goal
+//route : GET/api/goals
+//@ access : private
+
+
+
+const getGoals = (req, res) => {
+
+     res.status(200).json({ message: 'Get goals'})
+
+}
+
+//@desciption : get goal
+//route : GET/api/goals
+//@ access : private
+
+
+
+const getGoals = (req, res) => {
+
+     res.status(200).json({ message: 'Get goals'})
+
+}
+
+module.exports = {
+    getGoals,
+}
