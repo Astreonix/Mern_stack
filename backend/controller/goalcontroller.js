b4ee448954823a64c4612fc0ac6706f23b110ce6
Astreonix <sheikhmehrali5@gmail.com>
2026-07-04 18:46:41 +0500

    add config and controller

--- backend/controller/goalcontroller.js
+++ backend/controller/goalcontroller.js
@@ -1,29 +1,56 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../model/goalModel')
 
-const getGoals = asynchandler(async (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
+
+
+
+
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find({ user: req.user.id })
+
+    res.status(200).json(goals)
 })
 
-const setGoal = asynchandler(async (req, res) => {
-    if(!req.body.text) {
-        return res.status(400)
-
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400)
         throw new Error('Please add a text field')
     }
-
-     res.status(200).json({ message: 'set goals' })
+    res.status(200).json({ message: 'set goals' })
 })
 
-const updateGoal = asynchandler(async (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}` })
+const updateGoal = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    // Check for user
+    if (!req.user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })  
+
+    res.status(200).json(updatedGoal)
 })
 
-const getGoal = asynchandler(async (req, res) => {
+const getGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
 })
 
-const deleteGoal = asynchandler(async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `delete goal ${req.params.id}` })
 })
 
